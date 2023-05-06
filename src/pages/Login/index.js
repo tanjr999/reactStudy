569b94dc47ea5218e3bce1a1483469b9612af6d4
tanjr999 <2049163097@qq.com>
2023-05-06 16:52:48 +0800

    add: 新增登录页

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,19 @@
+import { Card } from "antd"
+
 function Login() {
     return (
-        <div>login</div>
+        <div className="login">
+            <Card className="login-container">
+                <div className="form-item">
+                    <label>账号：</label>
+                    <input type='text' placeholder="请输入昵称" />
+                </div>
+                <div className="form-item">
+                    <label>密码：</label>
+                    <input type='password' placeholder="请输入密码"/>
+                </div>
+            </Card>
+        </div>
     )
 }
 
