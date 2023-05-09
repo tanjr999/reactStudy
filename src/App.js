cff2560408503817c1c8d3e597ebae7377cf9dae
tanjr999 <2049163097@qq.com>
2023-05-09 14:14:05 +0800

    add: (新增页面)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import Login from './pages/Login/index'
 import Layout from './pages/Layout/index'
 import Login from '@/pages/Login/index'
-
-import {Button} from 'antd'
 
 function App() {
     return (
         // 路由配置
         <BrowserRouter>
             <div className='App'>
-                <Button type='primary'>按钮测试</Button>
                 <Routes>
                     <Route path='/' element={<Layout></Layout>}></Route>
                     <Route path='/login' element={<Login></Login>}></Route>
